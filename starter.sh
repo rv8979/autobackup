fa19f6c66154022cf811a7e97b732d3263fa1367
Raghav Chauhan <45090896+rv8979@users.noreply.github.com>
2019-10-10 09:55:16 +0530

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 _zenity="/usr/bin/zenity"
-_out="/tmp/whois.output.$$"
+_out="./whois.output.$$"
 _bash="/bin/bash"
-_script="/home/raghav/Desktop/cc/backup_script.sh"
+_script="./backup_script.sh"
 _pidof=""
 
 
 #------------
 # Check thunderbird is running
-_pidof=$(pidof thunderbirt)
+#------------------
+_pidof=$(pidof thunderbird)
 
-if {_pidof -ne ""} then 
- pkill -9 _pidof
+
+if [ $_pidof != "" ];
+then
+ps -ef | grep thunderbird | grep -v grep | awk '{print $2}'| xargs kill -9
+fi
+
 
 
 ${_bash} $_script  | tee >(${_zenity} --width=200 --height=100 \
-                          --title="Creating Emails
-
-
-
- Backup" --progress \
+                          --title="Creating Backup" --progress \
                           --pulsate --text="file copy" \
                           --auto-kill --auto-close --no-cancel \
                           --percentage=10) >${_out}
 
-cp $_out ~/Desktop/cc/
   ${_zenity} --width=800 --height=600  \
              --title "Backup Log" \
 	         --text-info --filename="${_out}"
